4055343ae2d822e8179fc606d5b46bec068a90d8
Cristián Inaudi <crinaudi@hotmail.com>
2021-07-29 15:52:14 -0300

    Cargar de Proyectos

---
+++ Pagina Principal Estancia La Granadilla/includes/templates/footer.php
@@ -0,0 +1,27 @@
+</body>
+<footer class="site-footer">
+    <div class="contenedor clearfix">
+        <div class="idioma barras-contorno">
+            <a href="#">Español</a>
+            <a href="#">English</a>    
+        </div>
+        <div class="galeria-imagenes barras-contorno">
+            <a href="#">Galería de Imágenes</a>
+        </div>
+        <div class="redes barras-contorno footer-der clearfix">
+            <a href="https://www.facebook.com/estancialagranadilla" target="_blank"><i class="fa fa-facebook-square"></i></a>
+            <a href="https://www.instagram.com/explore/locations/257083412/estancia-la-granadilla" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        <div class="contacto barras-contorno footer-der clearfix">
+            <a href="#">Contacto</a>
+        </div>
+    </div>
+
+
+
+</footer>
+
+    <script src="js/plugins.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
+
---
+++ Pagina Principal Estancia La Granadilla/includes/templates/header.php
@@ -0,0 +1,55 @@
+<head>
+    <meta charset="utf-8">
+    <title> Estancia Hotel La Granadilla</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <meta name="theme-color" content="#c3bc96">
+
+</head>
+
+<body>
+    <div class="barra">
+        <div class="contenedor clearfix">
+            <div class="logo">
+                <a href="index.php">
+                    <img src="img/logo-grana.png" alt="logo grana">
+                </a>
+            </div>
+
+            <div class="menu-movil">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <nav class="navegacion-principal">
+                <ul class="clearfix">
+                    <li>
+                        <a id="btn_vacaciones" href="vacaciones-familia.php">
+                            <span>Eventos Sociales</span>
+                        </a>
+                    </li>
+                    <li><a id="btn_empresas" href="eventos-empresariales.php"><span>Eventos Empresariales</span></a></li>
+                    <li><a id="btn_sociales" href="eventos-sociales.php"><span>Vacaciones en Familia</span></a></li>
+                </ul>  
+            </nav>
+        </div><!--.contenedor-->
+    </div><!--.barra-->
+    
+</body>
